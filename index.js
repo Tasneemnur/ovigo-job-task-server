3df20ac69af57928cb3d5b608776c25c666ad229
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-07-10 01:14:01 +0600

    jwt generated and fake data created and stored to database.

--- index.js
+++ index.js
@@ -10,6 +10,80 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const fakeData = [
+  {
+    "communityName": "The Betterment Society",
+    "communityPicture": "https://i.ibb.co/zx9MS2M/society.jpg",
+    "time": "12:30 PM",
+    "likes": 10,
+    "details": "Those of us that are here in Bangladesh have all heard and seen the devastating effects the flood has brought upon the people of the Greater Sylhet region. So many lives lost and countless livelihoods shelters destroyed. The whole city is in dire need of support. Those who are abroad probably with no idea what’s going on, we can only show you the pictures and videos of the situation here but you would have to be there to understand what it feels like to see your home, the city you grew up in get ruined in front of your own eyes.",
+    "postPicture": "https://i.ibb.co/P5kwcqF/post1.jpg"
+  },
+  {
+    "communityName": "Pink Blush",
+    "communityPicture": "https://i.ibb.co/Km3XnjQ/blush.jpg",
+    "time": "09:45 AM",
+    "likes": 5,
+    "details": "Check out the new Pink Blush Summer Collection! Get ready for the summer with our vibrant and stylish outfits. From flowy dresses to trendy tops, we have it all. Visit our website to browse the collection and enjoy exclusive discounts. Don't miss out on the perfect summer wardrobe! #PinkBlush #SummerCollection",
+    "postPicture": "https://i.ibb.co/Rb7qSbH/post2.jpg"
+  },
+  {
+    "communityName": "Love not War",
+    "communityPicture": "https://i.ibb.co/DfKnnM8/love.jpg",
+    "time": "06:15 PM",
+    "likes": 15,
+    "details": "Spread love, not war! Let's embrace kindness, compassion, and understanding. Together, we can make the world a better place. Share your stories of love, acts of kindness, or inspirational quotes in the comments below. Remember, a little love can go a long way! #LoveNotWar #SpreadLove",
+    "postPicture": "https://i.ibb.co/23K7H0L/post3.jpg"
+  },
+{
+    "communityName": "Science in Progress",
+    "communityPicture": "https://i.ibb.co/Kwd05Fx/science.jpg",
+    "time": "08:20 PM",
+    "likes": 30,
+    "details": "After President Obama’s call to attain a “level of research and development not seen since the height of the Space Race” in the State of the Union, universities are renewing their cry for a deal to avoid the automatic budget cuts known as sequestration.",
+    "postPicture": "https://i.ibb.co/p0FQZft/post4.jpg"
+  },
+{
+    "communityName": "Green Fields",
+    "communityPicture": "https://i.ibb.co/b2zMksK/field.jpg",
+    "time": "09:15 AM",
+    "likes": 12,
+    "details": "The greenfield strategy relies on a complete fresh start. To use a metaphor: The project starts anew and proverbially “on the greenfield”. This path has advantages. Often, many legacy systems have grown historically and have been subject to many adaptations over the course of the last few years.",
+    "postPicture": "https://i.ibb.co/BPML4WY/post5.jpg"
+  },
+{
+    "communityName": "The Betterment Society",
+    "communityPicture": "https://i.ibb.co/zx9MS2M/society.jpg",
+    "time": "5:30 PM",
+    "likes": 45,
+    "details": "Assalamualaikum everyone. Alhamdulillah we were able to hold two events with the funds raised. Thank you everyone for your love, support and prayers that led us to make #project_help a huge success in such a short period of time. After Eid-ul-Fitr, we did a collaboration with an organisation from Sunamganj named 'Swopnadorsho' স্বপ্নাদর্শ and distributed rations to around a 100 families during this difficult covid times.",
+    "postPicture": "https://i.ibb.co/ZMx6Hm7/post6.jpg"
+  },
+{
+    "communityName": "Love not War",
+    "communityPicture": "https://i.ibb.co/DfKnnM8/love.jpg",
+    "time": "12:12 AM",
+    "likes": 20,
+    "details": "Investing in a child's growth and development is crucial, as it sets a strong foundation for their future. Nurturing and guiding children through their formative years can help shape their character and potential. Remember, it is easier to build a child with love, support, and education than it is to repair an adult. Let's prioritize providing a positive and empowering environment for our children's growth. Together, we can help them flourish into confident and resilient individuals.",
+    "postPicture": "https://i.ibb.co/VD6q216/post7.jpg"
+  },
+{
+    "communityName": "Green Fields",
+    "communityPicture": "https://i.ibb.co/b2zMksK/field.jpg",
+    "time": "2:10 PM",
+    "likes": 14,
+    "details": "Take a moment to appreciate the beauty of a green field. There's something truly enchanting about the lush green grass dancing in the breeze. It symbolizes growth, renewal, and the wonders of nature. Let's cherish and protect our green spaces, ensuring they thrive for future generations to enjoy. Share your favorite green field photos or memories in the comments below! #GreenField #NatureLove",
+    "postPicture": "https://i.ibb.co/k8sPZ77/post8.jpg"
+  },
+{
+    "communityName": "The Betterment Society",
+    "communityPicture": "https://i.ibb.co/zx9MS2M/society.jpg",
+    "time": "4:30 PM",
+    "likes": 30,
+    "details": "Assalamualaikum all, After a successful fundraising Alhamdulilah, we decided to hold two events for  #project_help. On the 27th of Ramadan, we have successfully distributed Iftar to more than a 100 people! Here's a glimpse of the blessed day.",
+    "postPicture": "https://i.ibb.co/xYqLKw6/post9.jpg"
+  },
+]
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
@@ -45,6 +119,8 @@ async function run() {
     const usersCollection = client.db("travellersDB").collection("users");
     const communitiesCollection = client.db("travellersDB").collection("communities");
     const cartsCollection = client.db("travellersDB").collection("carts");
+    const postsCollection = client.db("travellersDB").collection("posts");
+    
   try {
     await client.connect();
 
@@ -55,7 +131,11 @@ async function run() {
       });
       res.send(token);
     });
-  
+    const result = await postsCollection.insertMany(fakeData);
+    app.get("/posts", async (req, res) => {
+      const result = await postsCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
