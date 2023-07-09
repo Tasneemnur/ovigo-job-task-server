ae8e1fd6bbe020387b1fc8d7dea02709c3412493
Tasneem Nur <tasneemnurtashnia@gmail.com>
2023-07-09 21:05:09 +0600

    api for jwt and also for the the specific carts element made.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require("express");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9iyox0a.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,13 +44,64 @@ const client = new MongoClient(uri, {
 async function run() {
     const usersCollection = client.db("travellersDB").collection("users");
     const communitiesCollection = client.db("travellersDB").collection("communities");
+    const cartsCollection = client.db("travellersDB").collection("carts");
   try {
     await client.connect();
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send(token);
+    });
   
     app.post("/users", async (req, res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
         res.send(result);
+      });
+      app.get("/users", async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
+      app.get("/communities", async (req, res) => {
+        const result = await communitiesCollection.find().toArray();
+        res.send(result);
+      });
+      app.get("/carts", async (req, res) => {
+        let query = {};
+        if (req.query?.email) {
+          query = { email: req.query.email };
+        }
+        if (req.query?.id) {
+          console.log(req.query.id)
+          query = { adminId: req.query.id };
+        }
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result);
+      });
+      app.patch("/carts/approved/:id", async (req, res) => {
+        const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateClass = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await cartsCollection.updateOne(filter, updateClass);
+      res.send(result);
+      })
+      app.post("/carts", async (req, res) => {
+        const cart = req.body;
+        const insertResult = await cartsCollection.insertOne(cart);
+
+        // const {communityId} = cart;
+        // const updateResult = await communitiesCollection.updateOne({ _id: new ObjectId(communityId) }, {
+        //   $inc: { members: 1 }
+        // },{ upsert: true } )
+  
+        res.send(insertResult);
       });
     app.post("/communities", async (req, res) => {
         const community = req.body;
